e3257af9164c3deeafbca67abdc02324d401f6e8
Alireza Sadraii Rad <sadraiiali@gmail.com>
2024-02-04 19:15:46 +0100

    fix libraries

--- appimage/build_appimage.sh
+++ appimage/build_appimage.sh
@@ -3,21 +3,30 @@ set -e
 sudo apt-get update && sudo apt-get install -y tzdata sudo gcc g++ wget flex bison libboost-all-dev automake make cmake iputils-ping
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib:/usr/lib:/lib
 
-./bootstrap
-./configure --prefix=$(pwd)/server-bin
+# ./bootstrap
+# ./configure --prefix=$(pwd)/server-bin
+# make
+# make install
+
+mkdir build
+cd build
+cmake ..
 make
-make install
+
 
 wget -c "https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage" -O linuxdeploy-x86_64.AppImage
 chmod +x linuxdeploy-x86_64.AppImage
 mkdir rcssserver-x86_64
 
 ./linuxdeploy-x86_64.AppImage --appdir ./rcssserver-x86_64 \
-                                -e ./server-bin/bin/rcssserver \
-                                -l ./server-bin/lib/librcssclangparser.so \
-                                -l ./server-bin/lib/librcssconfparser.so \
-                                -l ./server-bin/lib/librcssgz.so \
-                                -l ./server-bin/lib/librcssnet.so \
+                                -e ./build/rcssserver \
+                                -l ./build/clang/librcssclangparser.so \
+                                -l ./build/conf/librcssconfparser.so \
+                                -l ./build/gzip/librcssgz.so \
+                                -l ./build/net/librcssnet.so \
+                                -l /usr/lib/libc.so.6 \
+                                -l /usr/lib/libz.so.1 \
+                                -l /usr/lib/libstdc++.so.6 \
                                 -d ./appimage/rcssserver.desktop \
                                 -i ./appimage/rcssserver.png \
                                 --output appimage 
