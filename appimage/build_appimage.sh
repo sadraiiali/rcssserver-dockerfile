320510bd14174d1fcf98333076f198a3a6e1e770
Alireza Sadraii Rad <sadraiiali@gmail.com>
2024-02-04 21:29:47 +0100

    test

--- appimage/build_appimage.sh
+++ appimage/build_appimage.sh
@@ -10,7 +10,7 @@ mkdir rcssserver-x86_64
                                 -l ./build/rcss/clang/librcssclangparser.so.18.0.0 \
                                 -l ./build/rcss/conf/librcssconfparser.so.18.0.0 \
                                 -l ./build/rcss/gzip/librcssgz.so.1.0.0 \
-                                -l ./build/rcss/net/librcssnet.so.1.0.0 \
+                                -l ./build/rcss/net/librcssnet.so.1.0.1 \
                                 -l /usr/lib/libc.so.6 \
                                 -l /usr/lib/libz.so.1 \
                                 -l /usr/lib/libstdc++.so.6 \
