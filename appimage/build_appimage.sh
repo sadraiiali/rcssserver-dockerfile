1b0441275acfc800ef26a0a7e4d7114c097ba9cc
Alireza Sadraii Rad <sadraiiali@gmail.com>
2024-02-04 21:22:37 +0100

    test

--- appimage/build_appimage.sh
+++ appimage/build_appimage.sh
@@ -4,14 +4,13 @@ set -e
 wget -c "https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage" -O linuxdeploy-x86_64.AppImage
 chmod +x linuxdeploy-x86_64.AppImage
 mkdir rcssserver-x86_64
-echo $(pwd)
-ls -la
+
 ./linuxdeploy-x86_64.AppImage --appdir ./rcssserver-x86_64 \
                                 -e ./build/rcssserver \
-                                -l ./build/clang/librcssclangparser.so.18.0.0 \
-                                -l ./build/conf/librcssconfparser.so.18.0.0 \
-                                -l ./build/gzip/librcssgz.so.18.0.0 \
-                                -l ./build/net/librcssnet.so.1.0.0 \
+                                -l ./build/rcss/clang/librcssclangparser.so.18.0.0 \
+                                -l ./build/rcss/conf/librcssconfparser.so.18.0.0 \
+                                -l ./build/rcss/gzip/librcssgz.so.18.0.0 \
+                                -l ./build/rcss/net/librcssnet.so.1.0.0 \
                                 -l /usr/lib/libc.so.6 \
                                 -l /usr/lib/libz.so.1 \
                                 -l /usr/lib/libstdc++.so.6 \
