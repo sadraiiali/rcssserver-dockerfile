bb18d404b58bc3e58dd113bd783b832603ea896c
Alireza Sadraii Rad <sadraiiali@gmail.com>
2024-02-14 14:43:03 +0100

    chore: add build pwd

--- utils/appimage/build_appimage.sh
+++ utils/appimage/build_appimage.sh
@@ -5,16 +5,21 @@ wget -c "https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous
 chmod +x linuxdeploy-x86_64.AppImage
 mkdir rcssserver-x86_64
 
+BUILD_PWD='/rcssserver/build'
+
+
 # find libc and libstdc++ libz dependencies
-LIBSTDCPP_PATH=$(ldd ./build/rcssserver | grep libstdc++ | awk '{ print $3 }')
-LIBZ_PATH=$(ldd ./build/rcssserver | grep libz.so | awk '{ print $3 }')
-LIBRCSSCLANGPARSER_PATH=$(ldd ./build/rcssserver | grep librcssclangparser.so | awk '{ print $3 }')
-LIBRCSSCONFPARSER_PATH=$(ldd ./build/rcssserver | grep librcssconfparser.so | awk '{ print $3 }')
-LIBRCSSGZ_PATH=$(ldd ./build/rcssserver | grep librcssgz.so | awk '{ print $3 }')
-LIBRCSSNET_PATH=$(ldd ./build/rcssserver | grep librcssnet.so | awk '{ print $3 }')
+LIBSTDCPP_PATH=$(ldd $BUILD_PWD/build/rcssserver | grep libstdc++ | awk '{ print $3 }')
+LIBZ_PATH=$(ldd $BUILD_PWD/build/rcssserver | grep libz.so | awk '{ print $3 }')
+LIBRCSSCLANGPARSER_PATH=$(ldd $BUILD_PWD/build/rcssserver | grep librcssclangparser.so | awk '{ print $3 }')
+LIBRCSSCONFPARSER_PATH=$(ldd $BUILD_PWD/build/rcssserver | grep librcssconfparser.so | awk '{ print $3 }')
+LIBRCSSGZ_PATH=$(ldd $BUILD_PWD/build/rcssserver | grep librcssgz.so | awk '{ print $3 }')
+LIBRCSSNET_PATH=$(ldd $BUILD_PWD/build/rcssserver | grep librcssnet.so | awk '{ print $3 }')
+
+tree /rcssserver
 
 ./linuxdeploy-x86_64.AppImage --appdir ./rcssserver-x86_64 \
-                                -e ./build/rcssserver \
+                                -e $BUILD_PWD/build/rcssserver \
                                 -l $LIBRCSSCLANGPARSER_PATH \
                                 -l $LIBRCSSCONFPARSER_PATH \
                                 -l $LIBRCSSGZ_PATH \
