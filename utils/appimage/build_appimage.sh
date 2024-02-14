d7c37578f5f296703513e66482c02629e5d916a1
Alireza Sadraii Rad <sadraiiali@gmail.com>
2024-02-14 14:38:09 +0100

    chore: parameterized everything

--- utils/appimage/build_appimage.sh
+++ utils/appimage/build_appimage.sh
@@ -6,16 +6,19 @@ chmod +x linuxdeploy-x86_64.AppImage
 mkdir rcssserver-x86_64
 
 # find libc and libstdc++ libz dependencies
-LIBC_PATH=$(ldd ./build/rcssserver | grep libc.so | awk '{ print $3 }')
 LIBSTDCPP_PATH=$(ldd ./build/rcssserver | grep libstdc++ | awk '{ print $3 }')
 LIBZ_PATH=$(ldd ./build/rcssserver | grep libz.so | awk '{ print $3 }')
+LIBRCSSCLANGPARSER_PATH=$(ldd ./build/rcssserver | grep librcssclangparser.so | awk '{ print $3 }')
+LIBRCSSCONFPARSER_PATH=$(ldd ./build/rcssserver | grep librcssconfparser.so | awk '{ print $3 }')
+LIBRCSSGZ_PATH=$(ldd ./build/rcssserver | grep librcssgz.so | awk '{ print $3 }')
+LIBRCSSNET_PATH=$(ldd ./build/rcssserver | grep librcssnet.so | awk '{ print $3 }')
 
 ./linuxdeploy-x86_64.AppImage --appdir ./rcssserver-x86_64 \
                                 -e ./build/rcssserver \
-                                -l ./build/rcss/clang/librcssclangparser.so.18.0.0 \
-                                -l ./build/rcss/conf/librcssconfparser.so.18.0.0 \
-                                -l ./build/rcss/gzip/librcssgz.so.1.0.0 \
-                                -l ./build/rcss/net/librcssnet.so.1.0.1 \
+                                -l $LIBRCSSCLANGPARSER_PATH \
+                                -l $LIBRCSSCONFPARSER_PATH \
+                                -l $LIBRCSSGZ_PATH \
+                                -l $LIBRCSSNET_PATH \
                                 -l $LIBSTDCPP_PATH \
                                 -l $LIBZ_PATH \
                                 -d ./appimage/rcssserver.desktop \
